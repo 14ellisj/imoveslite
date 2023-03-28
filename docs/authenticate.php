ebe71b058c70bc49ad9ff2d2f50a6e280dcec2ac
14ellisj <jakeellis5000@gmail.com>
2023-03-28 17:43:05 +0100

    Test run A

--- docs/authenticate.php
+++ docs/authenticate.php
@@ -17,7 +17,6 @@ if ($stmt = $mysqli->prepare('SELECT id, password FROM accounts WHERE username =
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
 	$stmt->store_result();
-
     if ($stmt->num_rows > 0) {
         $stmt->bind_result($id, $password);
         $stmt->fetch();
@@ -27,15 +26,13 @@ if ($stmt = $mysqli->prepare('SELECT id, password FROM accounts WHERE username =
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['name'] = $_POST['username'];
             $_SESSION['id'] = $id;
-            header('Location: home.php');
+            header('Location: mainPage.html');
         } else {
             echo 'Incorrect username and/or password!';
         }
     } else {
         echo 'Incorrect username and/or password!';
     }
-
-
 	$stmt->close();
 }
 ?>
