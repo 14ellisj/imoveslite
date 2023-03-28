a790076d4b5d16764460adcfc2ccb929e1106c0b
14ellisj <jakeellis5000@gmail.com>
2023-03-28 16:01:40 +0100

    test2

--- docs/authenticate.php
+++ docs/authenticate.php
@@ -5,11 +5,11 @@ session_start();
 // $username = 'admin1';
 // $password = 'BOBicus1';
 // $database = 'pick-l';
-$con = mysqli_connect($hostname, $username, $password, $database);
-if ( mysqli_connect_errno() ) {
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-// include("config.php");
+// $con = mysqli_connect($hostname, $username, $password, $database);
+// if ( mysqli_connect_errno() ) {
+// 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+// }
+include("config.php");
 if ( !isset($_POST['username'], $_POST['password']) ) {
 	exit('Please fill both the username and password fields!');
 }
@@ -21,7 +21,8 @@ if ($stmt = $mysqli->prepare('SELECT id, password FROM accounts WHERE username =
     if ($stmt->num_rows > 0) {
         $stmt->bind_result($id, $password);
         $stmt->fetch();
-        if (password_verify($_POST['password'], $password)) {
+        // if (password_verify($_POST['password'], $password)) {
+            if($_POST['password'] == $password) {
             session_regenerate_id();
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['name'] = $_POST['username'];
--- docs/populate.php
+++ docs/populate.php
@@ -1,7 +1,14 @@
 <?php
 include("config.php");
+// $sql = "INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES 
+// (1, 'test', '$2y$10$SfhYIDtn.iOuCW7zfoFLuuZHX6lja4lF4XA4JqNmpiH/.P3zB8JCa', 'test@test.com');";
+// if ($mysqli->query($sql) === TRUE) {
+// 	echo "Table MyGuests created successfully";
+//   } else {
+// 	echo "Error creating table: " . $mysqli->error;
+//   }
 $sql = "INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES 
-(1, 'test', '$2y$10$SfhYIDtn.iOuCW7zfoFLuuZHX6lja4lF4XA4JqNmpiH/.P3zB8JCa', 'test@test.com');";
+(2, 'test2', 'test2', 'test2@test.com');";
 if ($mysqli->query($sql) === TRUE) {
 	echo "Table MyGuests created successfully";
   } else {
