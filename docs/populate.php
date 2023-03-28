7df667f16528d5f038e79b44f59b54a6c7e94143
14ellisj <jakeellis5000@gmail.com>
2023-03-28 15:28:53 +0100

    populate.1

--- docs/populate.php
+++ docs/populate.php
@@ -1,5 +1,10 @@
 <?php
-require_once("PHPLOGIN/authenticate.php");
+include("config.php");
 $sql = "INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES 
 (1, 'test', '$2y$10$SfhYIDtn.iOuCW7zfoFLuuZHX6lja4lF4XA4JqNmpiH/.P3zB8JCa', 'test@test.com');";
+if ($mysqli->query($sql) === TRUE) {
+	echo "Table MyGuests created successfully";
+  } else {
+	echo "Error creating table: " . $mysqli->error;
+  }
 ?>
