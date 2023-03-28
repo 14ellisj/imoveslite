c4666ae7c6a4ad6759b3f60956ac5dbfc46045ab
14ellisj <jakeellis5000@gmail.com>
2023-03-28 14:10:07 +0100

    php deploy

---
+++ docs/populate.php
@@ -0,0 +1,5 @@
+<?php
+require_once("PHPLOGIN/authenticate.php");
+$sql = "INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES 
+(1, 'test', '$2y$10$SfhYIDtn.iOuCW7zfoFLuuZHX6lja4lF4XA4JqNmpiH/.P3zB8JCa', 'test@test.com');";
+?>
