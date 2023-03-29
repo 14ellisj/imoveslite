c874f16c62a186de6c5d305f7b35fbce1d60597b
14ellisj <jakeellis5000@gmail.com>
2023-03-29 10:55:24 +0100

    profiles test 2.2

--- docs/profile.php
+++ docs/profile.php
@@ -33,15 +33,9 @@ $stmt->close(); */
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
 	</head>
 	<body class="loggedin">
-		<nav class="navtop">
-			<div>
-				<h1>Website Title</h1>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
-		</nav>
+
 		<div class="content">
-			<h2>Profile Page</h2>
+			<h2>Profile Information</h2>
 			<div>
 				<p>Your account details are below:</p>
 				<table>
@@ -51,14 +45,19 @@ $stmt->close(); */
 					</tr>
 					<tr>
 						<td>Password:</td>
-						<td><?=$password?></td>
+						<td><?=$_SESSION['password']?></td>
 					</tr>
 					<tr>
 						<td>Email:</td>
-						<td><?=$email?></td>
+						<td><?=$_SESSION['email']?></td>
 					</tr>
 				</table>
 			</div>
 		</div>
+
+    <div>
+        <a href="profiles.html"><button class="backButton"> ← Back </button></a>
+    </div>
+
 	</body>
 </html>
